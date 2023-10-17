c3a372cd53ac6e00f8c925edac6e831d12b342d0
riccardococciolo <coccioloriccardo64@gmail.com>
2023-10-17 16:20:23 +0200

    add: program logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// data-collection
 const name = prompt("Come ti chiami?");
 console.log(name);
 
@@ -6,3 +7,10 @@ console.log(surname);
 
 const color = prompt("Qual è il tuo colore preferito?");
 console.log(color);
+
+// program-logic
+const password = `${name}${surname}${color}21`;
+console.log(password);
+
+const message = `Questa è la tua password: ${password}`;
+console.log(password);
