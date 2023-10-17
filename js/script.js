a555e3fab5fe88d9448ac13250649e57bb51e5b2
riccardococciolo <coccioloriccardo64@gmail.com>
2023-10-17 15:59:39 +0200

    add: data collection

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const name = prompt("Come ti chiami?");
+console.log(name);
+
+const surname = prompt("Qual è il tuo cognome?");
+console.log(surname);
+
+const color = prompt("Qual è il tuo colore preferito?");
+console.log(color);
